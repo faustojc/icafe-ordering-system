b44eb6aff9acf286cee0cff557487db1a51bef9e
faustojc <faustojcboko@gmail.com>
2023-08-21 21:17:13 +0800

    Create Admin.php

---
+++ app/Models/Admin.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Admin extends Authenticatable
+{
+    use HasApiTokens;
+}
