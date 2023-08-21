43c681b78b284c5c2cdb1dbef9af12f8fab9a640
faustojc <faustojcboko@gmail.com>
2023-08-21 21:19:23 +0800

    Created ProductSeeder for fake data

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $products = [
+            [
+                'name' => 'AMERICANO',
+                'category' => 'COFFEE',
+                'price' => 90
+            ],
+            [
+                'name' => 'LATTE',
+                'category' => 'COFFEE',
+                'price' => 100
+            ],
+            [
+                'name' => 'COLDBREW',
+                'category' => 'COFFEE',
+                'price' => 100
+            ],
+            [
+                'name' => 'CHOCO',
+                'category' => 'COFFEE',
+                'price' => 100
+            ],
+            [
+                'name' => 'CARAMEL',
+                'category' => 'COFFEE',
+                'price' => 120
+            ],
+            [
+                'name' => 'RED HORSE',
+                'category' => 'BEER',
+                'price' => 70
+            ],
+            [
+                'name' => 'SAN MIG LIGHT',
+                'category' => 'BEER',
+                'price' => 70
+            ],
+            [
+                'name' => 'SMB',
+                'category' => 'BEER',
+                'price' => 70
+            ],
+            [
+                'name' => 'SM APPLE',
+                'category' => 'BEER',
+                'price' => 70
+            ],
+            [
+                'name' => 'ANTIDOTE',
+                'category' => 'COCKTAILS',
+                'price' => 80
+            ],
+            [
+                'name' => 'MEADOW',
+                'category' => 'COCKTAILS',
+                'price' => 80
+            ],
+        ];
+
+        foreach ($products as $product) {
+            DB::table('products')->insert([
+                'name' => $product['name'],
+                'category' => $product['category'],
+                'price' => $product['price'],
+                'image' => 'default.png'
+            ]);
+        }
+    }
+}
