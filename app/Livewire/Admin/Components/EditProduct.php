750ea5376e2095c84b28e775a36d867d4e83c690
faustojc <faustojcboko@gmail.com>
2023-08-21 21:16:48 +0800

    Created EditProduct component

---
+++ app/Livewire/Admin/Components/EditProduct.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\Components;
+
+use Livewire\Component;
+
+class EditProduct extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.components.edit-product');
+    }
+}
