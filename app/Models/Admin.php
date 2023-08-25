ef72cd8c1afab50eb909e4ac0609dc9bdfb7c9d5
faustojc <faustojcboko@gmail.com>
2023-08-25 23:05:37 +0800

    Added Notifiable trait

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class Admin extends Authenticatable
 {
-    use HasApiTokens;
+    use HasApiTokens, Notifiable;
 }
