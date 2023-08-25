7e6f3f42acd96274c76b6288b68e8967f373382b
faustojc <faustojcboko@gmail.com>
2023-08-25 23:03:04 +0800

    Added Broadcast channel for product event

--- routes/channels.php
+++ routes/channels.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Admin;
 use Illuminate\Support\Facades\Broadcast;
 
 /*
@@ -7,12 +8,16 @@ use Illuminate\Support\Facades\Broadcast;
 | Broadcast Channels
 |--------------------------------------------------------------------------
 |
-| Here you may register all of the event broadcasting channels that your
+| Here you may register all the event broadcasting channels that your
 | application supports. The given channel authorization callbacks are
 | used to check if an authenticated user can listen to the channel.
 |
 */
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+Broadcast::channel('App.Models.User.{id}', static function ($user, $id) {
+    return (int)$user->id === (int)$id;
 });
+
+Broadcast::channel('product-channel', static function (Admin $user) {
+    return TRUE;
+}, ['guards' => ['admin']]);
