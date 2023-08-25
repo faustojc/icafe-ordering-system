f353fee198ac61e9e9900e47f1ae8d440940f388
faustojc <faustojcboko@gmail.com>
2023-08-25 23:08:02 +0800

    Update image-upload.blade.php

--- resources/views/livewire/admin/components/product/image-upload.blade.php
+++ resources/views/livewire/admin/components/product/image-upload.blade.php
@@ -6,9 +6,9 @@
         <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
             <div class="flex flex-col items-center justify-center pt-5 pb-6">
                 <!-- Display preview image -->
-                @if ($image_file)
-                    <img src="{{ (strtoupper(gettype($image_file)) === 'STRING') ? $image_file : $image_file->temporaryUrl() }}" class="object-cover object-center rounded-lg" style="max-height: 180px" alt="" />
-                    <p class="mt-2 text-sm text-gray-500 dark:text-gray-400 truncate">{{ (strtoupper(gettype($image_file)) === 'STRING') ? $image_name : $image_file->getClientOriginalName() }}</p>
+                @if ($image)
+                    <img src="{{ (strtoupper(gettype($image)) === 'STRING') ? $image : $image->temporaryUrl() }}" class="object-cover object-center rounded-lg" style="max-height: 180px" alt="" />
+                    <p class="mt-2 text-sm text-gray-500 dark:text-gray-400 truncate">{{ (strtoupper(gettype($image)) === 'STRING') ? $image_name : $image->getClientOriginalName() }}</p>
                 @else
                     <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
@@ -30,11 +30,17 @@
                     </div>
                 </div>
             </div>
-            <input wire:model="image_file"
+            <input wire:model="image"
                    x-on:livewire-upload-start="uploading = true"
                    x-on:livewire-upload-finish="uploading = false; progress = 0"
                    x-on:livewire-upload-error="uploading = false"
                    x-on:livewire-upload-progress="progress = $event.detail.progress"
-                   id="dropzone-file" type="file" class="hidden" size="3000" accept=".png, .jpg, .jpeg" /> </label>
+                   id="dropzone-file" type="file" class="hidden" size="3000" accept=".png, .jpg, .jpeg" required />
+        </label>
+        @if($errors->has('image'))
+            <p class="mt-2 text-sm text-red-600 dark:text-red-500">
+                {{ $errors->first('image') }}
+            </p>
+        @endif
     </div>
 </div>
