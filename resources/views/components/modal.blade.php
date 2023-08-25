bffb5ac80e12c306ddf468ac307db728e9e494f8
faustojc <faustojcboko@gmail.com>
2023-08-25 23:07:41 +0800

    Changed the dispatch event value

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,4 +1,4 @@
-<div wire:ignore.self id="{{ $modal_id ?? '' }}" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+<div wire:ignore.self id="{{ $modal_id ?? '' }}" data-modal-backdrop="static" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative w-full max-w-2xl max-h-full">
         <!-- Modal content -->
         <div class="relative p-6 bg-white rounded-lg shadow dark:bg-gray-700">
@@ -8,7 +8,7 @@
                 <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
                     {{ $modal_title ?? '' }}
                 </h3>
-                <button data-modal-hide="{{ $modal_id ?? '' }}" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">
+                <button x-on:close-modal="$event.target.dispatchEvent(new Event('click'))" data-modal-hide="{{ $modal_id ?? '' }}" id="modal-close" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                     </svg>
