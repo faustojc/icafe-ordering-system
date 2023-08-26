ca5456fcc01b5146f4ab2306abe921ad8ded2dc8
faustojc <faustojcboko@gmail.com>
2023-08-27 00:48:38 +0800

    Added notification broadcast to recieve

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -9,4 +9,12 @@ use Laravel\Sanctum\HasApiTokens;
 class Admin extends Authenticatable
 {
     use HasApiTokens, Notifiable;
+
+    /**
+     * The channels the user receives notification broadcasts on.
+     */
+    public function receivesBroadcastNotificationsOn(): string
+    {
+        return 'admin.' . $this->id;
+    }
 }
