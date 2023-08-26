826c9b794449b120a16111dd4f170f930641366f
faustojc <faustojcboko@gmail.com>
2023-08-26 18:42:05 +0800

    Added logout

--- app/Livewire/Admin/Dashboard.php
+++ app/Livewire/Admin/Dashboard.php
@@ -5,11 +5,20 @@ namespace App\Livewire\Admin;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
+    public function logout(): RedirectResponse
+    {
+        auth()->guard('admin')->logout();
+        session()->invalidate();
+
+        return redirect()->route('admin.auth');
+    }
+
     #[Layout('livewire.layouts.app')]
     public function render(): View|\Illuminate\Foundation\Application|Factory|Application
     {
