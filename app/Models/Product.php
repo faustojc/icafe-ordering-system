f3043ef76928a8337da333adb97a32b7b759a91c
faustojc <faustojcboko@gmail.com>
2023-08-27 00:49:00 +0800

    Make Product model to broadcast events

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,18 +2,46 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Database\Eloquent\BroadcastsEvents;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Pagination\Paginator;
 
 class Product extends Model
 {
-    use HasFactory;
+    use BroadcastsEvents, HasFactory;
 
     public function orderItems(): HasMany
     {
         return $this->hasMany(OrderItem::class);
+    }
+
+    /**
+     * Get the channels that model events should broadcast on.
+     *
+     * @return array<int, Channel|Model>
+     */
+    public function broadcastOn(string $event): array
+    {
+        $admin = auth()->guard('admin')->user();
+
+        // Return a private channel for the authenticated admin
+        return [new PrivateChannel('App.Models.Admin.' . $admin->id)];
+    }
+
+    public function broadcastAs(string $event): string
+    {
+        return 'product-processed';
+    }
+
+    public function broadcastWith(string $event): array
+    {
+        return [
+            'admin' => auth('admin')->user()->id,
+            'product' => $this,
+            'type' => $event,
+        ];
     }
 }
