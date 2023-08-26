ed4efaa49edfdaf51f052c7b60aa371d64f563c6
faustojc <faustojcboko@gmail.com>
2023-08-26 18:44:02 +0800

    Uncommented for sanctum's request api

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -27,6 +27,7 @@ use Illuminate\Routing\Middleware\ThrottleRequests;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 class Kernel extends HttpKernel
 {
@@ -63,8 +64,8 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            ThrottleRequests::class.':api',
+            EnsureFrontendRequestsAreStateful::class,
+            ThrottleRequests::class . ':api',
             SubstituteBindings::class,
         ],
     ];
