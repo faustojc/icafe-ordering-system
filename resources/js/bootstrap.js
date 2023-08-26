6ca4fd933865fc93e5894bce42ccace4c9005b67
faustojc <faustojcboko@gmail.com>
2023-08-27 00:49:42 +0800

    Removed the Echo

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -5,27 +5,7 @@
  */
 
 import axios from 'axios';
-import Echo from 'laravel-echo';
-
-import Pusher from 'pusher-js';
 
 window.axios = axios;
-
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.withCredentials = true;
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-window.Pusher = Pusher;
-
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    forceTLS: true,
-    encrypted: true,
-});
