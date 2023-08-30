e81149f80e96612a4fe1132cabb62e46468e94b2
faustojc <faustojcboko@gmail.com>
2023-08-30 19:31:25 +0800

    Initialize inertia app with hydration for ssr

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,17 @@
+import {createInertiaApp} from '@inertiajs/react';
 import 'flowbite';
+import {hydrateRoot} from "react-dom";
 import './admin/notification.js';
 import './bootstrap';
+
+createInertiaApp({
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.jsx', {eager: true})
+        return pages[`./Pages/${name}.jsx`]
+    },
+    setup({el, App, props}) {
+        hydrateRoot(el, <App {...props} />)
+    },
+}).then(r => {
+    console.log('Inertia app setup complete.');
+})
