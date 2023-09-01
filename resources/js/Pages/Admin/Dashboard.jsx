f146ba5d822118be3ff94823daa33c6f7ce891c7
faustojc <faustojcboko@gmail.com>
2023-09-01 23:36:59 +0800

    Update Dashboard.jsx

--- resources/js/Pages/Admin/Dashboard.jsx
+++ resources/js/Pages/Admin/Dashboard.jsx
@@ -1,10 +1,12 @@
+import ShowOrders from "@/Admin/Orders/ShowOrders";
+import ShowProducts from "@/Admin/Products/ShowProducts";
+import {Head} from "@inertiajs/react";
+import {Card} from "flowbite-react";
 import {useState} from "react";
-import AddProductModal from "../../Pages/Admin/Products/AddProductModal.jsx";
+import AddProductModal from "../../Pages/Admin/Products/AddProductModal";
 import Sidebar from "../Components/Sidebar";
-import ShowOrders from "./Orders/ShowOrders";
-import ShowProducts from "./Products/ShowProducts";
 
-export default function Dashboard() {
+export default function Dashboard({ token, userId }) {
     const [activeTab, setActiveTab] = useState("orders");
     const [query, setQuery] = useState("");
 
@@ -16,6 +18,12 @@ export default function Dashboard() {
 
     return (
         <>
+            <Head>
+                <title>Dashboard</title>
+                <meta name={"api-token"} content={token} />
+                <meta name={"userId"} content={userId} />
+            </Head>
+
             <button data-drawer-target="sidebar" data-drawer-toggle="sidebar" aria-controls="sidebar" type="button"
                     className={"inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"}
             >
@@ -25,10 +33,10 @@ export default function Dashboard() {
                 </svg>
             </button>
 
-            <Sidebar setActiveTab={setActiveTab} />
+            <Sidebar activeTab={activeTab} setActiveTab={setActiveTab} />
 
             <div id={"tab_content"} className={"p-4 sm:ml-64"}>
-                <div className={"p-4 bg-white block sm:flex sm:items-center sm:justify-between border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700"}>
+                <Card className={"mb-4"}>
                     <div className={"w-full mb-1"}>
                         <div className={"mb-1"}>
                             <h1 className={"text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white"}>
@@ -51,20 +59,15 @@ export default function Dashboard() {
                             </div>
                             <button data-modal-toggle="add_product_modal"
                                     id={"product_modal_btn"} type={"button"}
-                                    className={"text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"}
+                                    className={"text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"}
                             >
                                 Add Product
                             </button>
                         </div>
                     </div>
+                </Card>
 
-                    <div id={"orders-panel"} role={"tabpanel"} aria-labelledby={"orders-tab"}>
-                        <ShowOrders />
-                    </div>
-                    <div id={"products-panel"} role={"tabpanel"} aria-labelledby={"products-tab"}>
-                        <ShowProducts query={query} />
-                    </div>
-                </div>
+                {activeTab === "orders" ? <ShowOrders /> : <ShowProducts query={query} /> }
             </div>
 
             <AddProductModal />
