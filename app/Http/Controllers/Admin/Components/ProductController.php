697bc45672abc1dad9ca6605854e46e778032afc
faustojc <faustojcboko@gmail.com>
2023-09-03 22:35:05 +0800

    Added functionalities

--- app/Http/Controllers/Admin/Components/ProductController.php
+++ app/Http/Controllers/Admin/Components/ProductController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Admin\Components;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use App\Notifications\ProductNotification;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use JsonException;
 
 class ProductController extends Controller
@@ -17,19 +19,33 @@ class ProductController extends Controller
     public function index(Request $request)
     {
         $query = $request->input('query');
-        $products = Product::query()->where('name', 'LIKE', "%{$query}%")->latest()->paginate(10);
+        $page = (is_null($request->input('page'))) ? 1 : (int)$request->input('page');
 
-        return json_encode([
-            'products' => $products,
-        ], JSON_THROW_ON_ERROR);
+        $products = Product::query()->where('name', 'LIKE', "%{$query}%")->latest()->paginate(perPage: 10, page: $page);
+
+        return json_encode($products, JSON_THROW_ON_ERROR);
     }
 
     /**
-     * Show the form for creating a new resource.
+     * @throws JsonException
      */
-    public function create()
+    public function upload(Request $request)
     {
-        //
+        $image = $request->file('image');
+        $validator = Validator::make(['image' => $image], [
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        if ($validator->fails()) {
+            return json_encode([
+                'message' => $validator->errors()->first(),
+            ], JSON_THROW_ON_ERROR);
+        }
+
+        return json_encode([
+            'message' => 'Valid image',
+            'image' => $image->getClientOriginalName(),
+        ], JSON_THROW_ON_ERROR);
     }
 
     /**
@@ -39,39 +55,68 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        $data = $request->all();
-        $data['image'] = $request->file('image')->storePubliclyAs('products', $request->file('image')->getClientOriginalName(), 'public');
+        $request->file('image')->storePubliclyAs('products', $request->file('image')->getClientOriginalName());
 
-        $product = Product::query()->create($data);
+        $product = new Product();
+        $product->name = $request->input('name');
+        $product->price = (float)$request->input('price');
+        $product->category = $request->input('category');
+        $product->description = $request->input('description');
+        $product->image = $request->file('image')->getClientOriginalName();
+        $product->save();
 
-        return json_encode([
-            'message' => 'Product created successfully',
-            'product' => $product,
-        ], JSON_THROW_ON_ERROR);
+        $addedProduct = Product::query()->where('id', $product->id)->first();
+
+        auth()->guard('admin')->user()->notify(new ProductNotification($addedProduct, 'Added', 'success'));
+
+        return json_encode($addedProduct, JSON_THROW_ON_ERROR);
     }
 
     /**
      * Display the specified resource.
+     *
+     * @throws JsonException
      */
     public function show(string $id)
     {
-        return Product::query()->find($id);
-    }
+        $product = Product::query()->where('id', $id)->first();
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
+        return json_encode($product, JSON_THROW_ON_ERROR);
     }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @throws JsonException
      */
     public function update(Request $request, string $id)
     {
-        //
+        $data = $request->all();
+
+        if ($request->hasFile('image')) {
+            $request->file('image')->storePubliclyAs('products', $request->file('image')->getClientOriginalName());
+            $data['image'] = $request->file('image')->getClientOriginalName();
+        }
+        else {
+            $data['image'] = Product::query()->where('id', $id)->get('image')->first()->image;
+        }
+
+        $product = Product::query()->find($id);
+        $product->name = $data['name'];
+        $product->price = (float)$data['price'];
+        $product->category = $data['category'];
+        $product->is_available = (int)$data['is_available'];
+        $product->featured = (int)$data['featured'];
+        $product->description = $data['description'];
+        $product->image = $data['image'];
+        $product->save();
+
+        auth()->guard('admin')->user()->notify(new ProductNotification($product, 'Updated', 'success'));
+
+        return json_encode([
+            'message' => 'Product updated successfully',
+            'product' => $product,
+        ], JSON_THROW_ON_ERROR);
     }
 
     /**
@@ -81,7 +126,10 @@ class ProductController extends Controller
      */
     public function destroy(string $id)
     {
-        Product::query()->where('id', $id)->delete();
+        $product = Product::query()->where('id', $id)->get()->first();
+
+        auth()->guard('admin')->user()->notify(new ProductNotification($product, 'Deleted', 'success'));
+        Product::query()->where('id', $id)->get()->first()->delete();
 
         return json_encode([
             'message' => 'Product deleted successfully',
