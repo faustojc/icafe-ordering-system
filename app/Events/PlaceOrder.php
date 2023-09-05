c5ef9790692c71b7d814b943f79c25f39448de70
faustojc <faustojcboko@gmail.com>
2023-09-05 23:48:45 +0800

    Renamed to PlaceOrder

--- app/Events/PlaceOrder.php
+++ app/Events/PlaceOrder.php
@@ -5,31 +5,31 @@ namespace App\Events;
 use App\Models\Order;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class OrderProcessed
+class PlaceOrder implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public Order $order;
 
     /**
      * Create a new event instance.
      */
-    public function __construct(
-        public Order $order,
-    )
+    public function __construct(Order $order)
     {
+        $this->order = $order;
     }
 
     /**
      * Get the channels the event should broadcast on.
      *
-     * @return array<int, Channel>
+     * @return Channel
      */
-    public function broadcastOn(): array
+    public function broadcastOn(): Channel
     {
-        return [
-            new Channel('order-channel'),
-        ];
+        return new Channel('place-order');
     }
 }
