de46274d456c1bac6af10427f17c41b7ed8d7abb
faustojc <faustojcboko@gmail.com>
2023-09-07 23:41:58 +0800

    Change layout for order navbar

--- resources/js/Pages/Components/OrderNavbar.jsx
+++ resources/js/Pages/Components/OrderNavbar.jsx
@@ -1,16 +1,19 @@
-import {Navbar} from "flowbite-react";
-
-export default function OrderNavbar() {
+export default function OrderNavbar({ children }) {
     return (
-        <>
-            <Navbar fluid rounded>
-                <Navbar.Brand>
-                    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-                        Flowbite React
-                    </span>
-                </Navbar.Brand>
-                
-            </Navbar>
-        </>
+
+        <nav className="bg-white border-gray-200 dark:bg-gray-900">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <a href="https://flowbite.com/" className="flex items-center">
+                    <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                </a>
+                <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                    <span className="sr-only">Open cart</span>
+                    <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
+                    </svg>
+                </button>
+            </div>
+        </nav>
     );
 }
