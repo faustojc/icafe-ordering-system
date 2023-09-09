6f9e6f67b98e7082e7e03023f885151b26af82f9
faustojc <faustojcboko@gmail.com>
2023-09-09 09:04:00 +0800

    Update layout and added filter

--- resources/js/Pages/Components/FilterCard.jsx
+++ resources/js/Pages/Components/FilterCard.jsx
@@ -1,23 +1,26 @@
 import {Card} from "flowbite-react";
 
-export default function FilterCard() {
+export default function FilterCard({ handleChange, className }) {
     return (
-        <>
-            <Card>
-                <h3>Filter</h3>
-                <h5>Category</h5>
-                <div className="flex items-center mb-4">
-                    <input id="coffee" type="checkbox" value={"COFFEE"}
+        <Card className={className}>
+            <h5 className={"text-xl font-bold dark:text-white"}>Categories</h5>
+            <div className={"mb-4"}>
+                <div className={"flex items-center mb-4"}>
+                    <input onChange={handleChange} id="coffee" type="checkbox" value={"COFFEE"}
                            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
                     <label htmlFor="coffee" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Coffee</label>
-                    <input id="beer" type="checkbox" value={"BEER"}
+                </div>
+                <div className={"flex items-center mb-4"}>
+                    <input onChange={handleChange} id="beer" type="checkbox" value={"BEER"}
                            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
                     <label htmlFor="beer" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Beer</label>
-                    <input id="cocktail" type="checkbox" value={"COCKTAIL"}
+                </div>
+                <div className={"flex items-center mb-4"}>
+                    <input onChange={handleChange} id="cocktail" type="checkbox" value={"COCKTAIL"}
                            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
                     <label htmlFor="cocktail" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Cocktail</label>
                 </div>
-            </Card>
-        </>
+            </div>
+        </Card>
     );
 }
