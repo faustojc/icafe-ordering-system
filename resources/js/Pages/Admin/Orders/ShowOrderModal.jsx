2fe4e064cbd6343e2bf370df8b556439b331130d
faustojc <faustojcboko@gmail.com>
2023-09-22 10:23:16 +0800

    Fixed modal footer layout and renamed order

--- resources/js/Pages/Admin/Orders/ShowOrderModal.jsx
+++ resources/js/Pages/Admin/Orders/ShowOrderModal.jsx
@@ -1,18 +1,18 @@
 import {Modal} from "flowbite-react";
 import {useState} from "react";
 
-export default function ShowOrderModal({ customerName, order, setOrder, openOrderModal, setOpenOrderModal }) {
+export default function ShowOrderModal({ customerName, setOrders, selectedOrder, openOrderModal, setOpenOrderModal }) {
     const [loading, setLoading] = useState(false);
 
-    const numItems = (order.order_items || []).length;
+    const numItems = (selectedOrder.order_items || []).length;
 
     const handleServe = () => {
         setLoading(true);
 
-        window.axios.delete(`/api/orders/${order.id}`).then(() => {
+        window.axios.delete(`/api/orders/${selectedOrder.id}`).then(() => {
             // fetch the updated orders
             window.axios.get('/api/orders')
-                .then(response => setOrder(response.data));
+                .then(response => setOrders(response.data));
 
             setOpenOrderModal(false);
         }).finally(() => setLoading(false));
@@ -24,7 +24,7 @@ export default function ShowOrderModal({ customerName, order, setOrder, openOrde
                 {customerName} ({numItems} item/s)
             </Modal.Header>
             <Modal.Body>
-                <p className={"text-sm text-gray-400 mb-6"}>NOTES: {order.notes ? order.notes : 'No notes'}</p>
+                <p className={"text-sm text-gray-400 mb-6"}>NOTES: {selectedOrder.notes ? selectedOrder.notes : 'No notes'}</p>
                 <div className={"relative overflow-x-auto shadow-md sm:rounded-lg"}>
                     <table className={"w-full text-sm text-left text-gray-500 dark:text-gray-400"}>
                         <thead className={"text-xs text-gray-700 uppercase bg-blue-400 dark:bg-blue-800 dark:text-gray-400"}>
@@ -36,12 +36,12 @@ export default function ShowOrderModal({ customerName, order, setOrder, openOrde
                         </tr>
                         </thead>
                         <tbody>
-                        {order.order_items && order.order_items.map((item, index) => (
-                            <tr key={index} className={"bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"}>
+                        {selectedOrder.order_items && selectedOrder.order_items.map((item, index) => (
+                            <tr key={index} className={"bg-white border-b dark:bg-gray-800 dark:border-gray-700"}>
                                 <td className={"text-left px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"}>{item.product.name}</td>
                                 <td className={"text-left px-6 py-4"}>{item.quantity}</td>
                                 <td className={"text-left px-6 py-4"}>P {item.product.price}</td>
-                                <td className={"text-left px-6 py-4"}>P {item.product.price * item.quantity}</td>
+                                <td className={"font-bold text-blue-500 text-left px-6 py-4"}>P {item.product.price * item.quantity}</td>
                             </tr>
                         ))}
                         </tbody>
@@ -49,32 +49,31 @@ export default function ShowOrderModal({ customerName, order, setOrder, openOrde
                 </div>
             </Modal.Body>
             <Modal.Footer>
-                <div className={"grid grid-cols-2 mb-2"}>
-                    <h5 className={"font-medium text-md lg:text-lg text-dark dark:text-white"}>
-                        Total
+                <div className={"flex flex-col w-full"}>
+                    <h5 className={"mb-4 font-medium text-md lg:text-lg text-dark dark:text-white"}>
+                        Total: P {selectedOrder.total_price}
                     </h5>
-                    <h5 className={"font-medium text-md lg:text-lg text-dark dark:text-white"}>
-                        P {order.total_price}
-                    </h5>
-                </div>
-                <button className={"flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"}
-                        onClick={handleServe}
-                >
-                    {loading && (
-                        <span role="status" className={"flex items-center mr-2"}>
+                    <div className={"flex w-full"}>
+                        <button className={"flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"}
+                                onClick={handleServe}
+                        >
+                            {loading && (
+                                <span role="status" className={"flex items-center mr-2"}>
                             <svg aria-hidden="true" role="status" className="inline w-4 h-4 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
                                 <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
                             </svg>
                         </span>
-                    )}
-                    Serve
-                </button>
-                <button onClick={() => setOpenOrderModal(false)}
-                        className={"py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"}
-                >
-                    Close
-                </button>
+                            )}
+                            Serve
+                        </button>
+                        <button onClick={() => setOpenOrderModal(false)}
+                                className={"py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"}
+                        >
+                            Close
+                        </button>
+                    </div>
+                </div>
             </Modal.Footer>
         </Modal>
     );
