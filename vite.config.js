1f596e5b60ee8aa5b853a1d4bd6f566cc0f9dc03
faustojc <faustojcboko@gmail.com>
2023-09-22 10:24:01 +0800

    Fixed app.jsx not found and remove ssr

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,7 @@ export default defineConfig({
     },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js',],
-            //ssr: 'resources/js/ssr.js',
+            input: ['resources/css/app.css', 'resources/js/app.jsx',],
             refresh: true,
         }),
         react(),
