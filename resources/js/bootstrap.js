a97539b40d240ad344120d7e4596aa7737627b8b
faustojc <faustojcboko@gmail.com>
2023-09-23 18:29:23 +0800

    Replace Echo in using public

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -5,7 +5,7 @@
  */
 
 import axios from 'axios';
-import Echo from 'laravel-echo';
+import Echo from "laravel-echo";
 import Pusher from 'pusher-js';
 
 window.axios = axios;
@@ -13,7 +13,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.withCredentials = true;
 
 window.Pusher = Pusher;
-window.Echo = new Echo({
+window.EchoPublic = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     wsHost: import.meta.env.VITE_PUSHER_HOST,
