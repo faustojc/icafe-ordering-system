f58646dd6ccb6fe3641f0e84967550a6d0b0a315
faustojc <faustojcboko@gmail.com>
2023-09-25 20:45:52 +0800

    Remove filter button

--- resources/js/Pages/Components/SearchCard.jsx
+++ resources/js/Pages/Components/SearchCard.jsx
@@ -1,6 +1,4 @@
-import {Card} from "flowbite-react";
-
-export default function SearchCard({ openFilter, setOpenFilter, setQuery }) {
+export default function SearchCard({ category, openFilter, setOpenFilter, setQuery }) {
     const handleKeyDown = (event) => {
         if (event.key === 'Enter') {
             setQuery(event.target.value);
@@ -8,30 +6,19 @@ export default function SearchCard({ openFilter, setOpenFilter, setQuery }) {
     }
 
     return (
-        <Card className={"w-full"}>
-            <div className={"flex gap-4"}>
-                <button onClick={() => setOpenFilter(!openFilter)}
-                        type="button"
-                        className="inline-flex md:hidden text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                    <svg className="w-3.5 h-3.5 mr-2 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m2.133 2.6 5.856 6.9L8 14l4 3 .011-7.5 5.856-6.9a1 1 0 0 0-.804-1.6H2.937a1 1 0 0 0-.804 1.6Z"/>
+        <>
+            <div className={"relative w-full md:w-1/2"}>
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
+                    <svg className="w-3.5 h-3.5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                     </svg>
-                    Filters
-                </button>
-                <div className={"relative grow"}>
-                    <div className="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
-                        <svg className="w-3.5 h-3.5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                        </svg>
-                    </div>
-                    <input onKeyDown={handleKeyDown}
-                           type="text" id="query" name="query"
-                           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                           placeholder="Search products..."
-                    />
                 </div>
+                <input onKeyDown={handleKeyDown}
+                       type="text" id="query" name="query"
+                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                       placeholder={`Search ${category.toLowerCase()}...`}
+                />
             </div>
-        </Card>
+        </>
     );
 }
