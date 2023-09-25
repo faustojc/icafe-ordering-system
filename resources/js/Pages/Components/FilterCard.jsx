2132c945040b8fa8cad36b5e149050c0c2bc1f42
faustojc <faustojcboko@gmail.com>
2023-09-25 20:45:00 +0800

    Adjusted UI and change category

--- resources/js/Pages/Components/FilterCard.jsx
+++ resources/js/Pages/Components/FilterCard.jsx
@@ -1,27 +1,33 @@
 import {Card} from "flowbite-react";
 
-export default function FilterCard({ categories = [], handleChange, className }) {
+export default function FilterCard({ category = '', setCategory, className }) {
     return (
         <Card className={className}>
-            <h5 className={"text-xl font-bold dark:text-white"}>Categories</h5>
-            <div className={"mb-4"}>
-                <div className={"flex items-center mb-4"}>
-                    <input onChange={handleChange} id="coffee" type="checkbox" value={"COFFEE"}
-                           checked={categories.includes("COFFEE")}
-                           className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-                    <label htmlFor="coffee" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Coffee</label>
+            <h5 className={"hidden md:block text-xl font-bold dark:text-white"}>Categories</h5>
+            <div className={"flex flex-row justify-around md:block"}>
+                <div onClick={() => setCategory('COFFEE')}
+                     className={"flex items-center flex-col md:flex-row md:mb-2 px-3 md:py-2 rounded-lg cursor-pointer " + (category === 'COFFEE' && 'bg-blue-200 dark:bg-blue-700')}
+                >
+                    <div>
+                        <img src={"/images/categories/coffee_icon.png"} className={"w-10 h-10"} alt={''} />
+                    </div>
+                    <p className="md:ml-2 text-sm font-bold text-gray-900 dark:text-gray-300">Coffee</p>
                 </div>
-                <div className={"flex items-center mb-4"}>
-                    <input onChange={handleChange} id="beer" type="checkbox" value={"BEER"}
-                            checked={categories.includes("BEER")}
-                           className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-                    <label htmlFor="beer" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Beer</label>
+                <div onClick={() => setCategory('BEER')}
+                     className={"flex items-center flex-col md:flex-row md:mb-2 px-3 md:py-2 rounded-lg cursor-pointer " + (category === 'BEER' && 'bg-blue-200 dark:bg-blue-700')}
+                >
+                    <div>
+                        <img src={"/images/categories/beer_icon.png"} className={"w-10 h-10"} alt={''} />
+                    </div>
+                    <p className="md:ml-2 text-sm font-bold text-gray-900 dark:text-gray-300">Beer</p>
                 </div>
-                <div className={"flex items-center mb-4"}>
-                    <input onChange={handleChange} id="cocktail" type="checkbox" value={"COCKTAIL"}
-                            checked={categories.includes("COCKTAIL")}
-                           className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-                    <label htmlFor="cocktail" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Cocktail</label>
+                <div onClick={() => setCategory('COCKTAIL')}
+                     className={"flex items-center flex-col md:flex-row px-3 md:py-2 rounded-lg cursor-pointer " + (category === 'COCKTAIL' && 'bg-blue-200 dark:bg-blue-700')}
+                >
+                    <div>
+                        <img src={"/images/categories/cocktail_icon.png"} className={"w-10 h-10"} alt={''} />
+                    </div>
+                    <p className="md:ml-2 text-sm font-bold text-gray-900 dark:text-gray-300">Cocktail</p>
                 </div>
             </div>
         </Card>
