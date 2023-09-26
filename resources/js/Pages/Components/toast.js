3650796cb1ef1e9eab200eb351e6c3ba8c33abd6
faustojc <faustojcboko@gmail.com>
2023-09-27 00:34:14 +0800

    Update toast.js

--- resources/js/Pages/Components/toast.js
+++ resources/js/Pages/Components/toast.js
@@ -3,13 +3,13 @@ export function showToast(message, type = '') {
 
     const toast = document.createElement('div');
     toast.id = toastId;
-    toast.className = `fixed bottom-5 right-5 flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800`;
+    toast.className = `flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800`;
     toast.role = 'alert';
 
     toast.innerHTML = `
     <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 rounded-lg"></div>
     <div class="ml-3 text-sm font-normal">${message}</div>
-    <button data-dismiss-target="#${toastId}" aria-label="Close" type="button" class="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">
+    <button id="${toastId}" aria-label="Close" type="button" class="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">
         <span class="sr-only">Close</span>
         <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
@@ -52,16 +52,14 @@ export function showToast(message, type = '') {
             break;
     }
 
-    document.querySelector('#notification').insertBefore(toast, document.querySelector('#notification').firstChild);
+    document.querySelector('#notification').insertAdjacentElement('beforeend', toast);
 
     toast.classList.add('transition', 'transition-opacity');
     setTimeout(() => {
         toast.remove();
     }, 5000);
 
-    const closeButton = toast.querySelector('[data-dismiss-target]');
+    const closeButton = toast.querySelector(`#${toastId}`);
 
-    closeButton.addEventListener('click', () => {
-        toast.remove();
-    });
+    closeButton.addEventListener('click', () => toast.remove());
 }
