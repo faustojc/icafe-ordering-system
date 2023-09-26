4b363d4f2f702b78e8024b865f1906db6c353673
faustojc <faustojcboko@gmail.com>
2023-09-27 00:33:05 +0800

    Removed broadcasting events

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,16 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Database\Eloquent\BroadcastsEvents;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Product extends Model
 {
-    use BroadcastsEvents, HasFactory;
+    use HasFactory;
 
     protected $fillable = [
         'id',
@@ -24,32 +21,5 @@ class Product extends Model
     public function orderItem(): HasOne
     {
         return $this->hasOne(OrderItem::class);
-    }
-
-    /**
-     * Get the channels that model events should broadcast on.
-     *
-     * @return array<int, Channel|Model>
-     */
-    public function broadcastOn(string $event): array
-    {
-        $admin = auth()->guard('admin')->user();
-
-        // Return a private channel for the authenticated admin
-        return [new PrivateChannel('App.Models.Admin.' . $admin->id)];
-    }
-
-    public function broadcastAs(string $event): string
-    {
-        return 'product-processed';
-    }
-
-    public function broadcastWith(string $event): array
-    {
-        return [
-            'admin' => auth('admin')->user()->id,
-            'product' => $this,
-            'type' => $event,
-        ];
     }
 }
